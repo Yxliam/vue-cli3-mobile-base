20407abcbe22ec910bd32e8abd1c44037aa06ee5
yxl <676557432@qq.com>
2019-04-16 11:57:39 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<div id="app">
+			<router-view/>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "App",
+		components: {
+		}
+	};
+</script>
+
+<style lang="scss">
+    @import "./style/reset.scss";
+    @import "./style/common.scss";
+    @import "~@/assets/iconfont/iconfont.css";
+    #app {
+        font-size: $font-size-base;
+        color: $color-common;
+        font-family: -apple-system, BlinkMacSystemFont, "PingFang SC", "SF UI Text",
+        "Helvetica Neue", STHeiti, "Microsoft Yahei", Tahoma, Simsun, sans-serif;
+    }
+</style>
